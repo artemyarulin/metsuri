eb7e6865c367d0fcdb3b6ab2abf43baac6705844
Artem Yarulin <Artem.Yarulin@fessguid.com>
2015-02-05 13:56:20 +0200

    Externs updated for transports

--- src/metsuri.extern.js
+++ src/metsuri.extern.js
@@ -6,8 +6,9 @@
 /** 
  * @constructor 
  * @param {string} minSeverity Minimum siverty. One of the following: trace,debug,warn,error 
+ * @param {Array.<Object>} transports Array of transports that would be used for log processing. Default is ConsoleTransport
  */
-var Metsuri = function(minSeverity){}
+var Metsuri = function(minSeverity,transports){}
 
 /**
  * Add context keys to each log enty
@@ -46,3 +47,29 @@ Metsuri.prototype.warn = function(sender,action,object){}
  * @param  {Object} object Log entry context
  */
 Metsuri.prototype.error = function(sender,action,object){}
+
+Metsuri.Transports = {}
+/**
+ * Will output everything to console
+ * @constructor
+ * @param {function(string): string=} preProcessFunction Optional function which can change string before the output to console 
+ */
+Metsuri.Transports.ConsoleTransport = function(preProcessFunction){}
+
+/**
+ * Will send data using POST request to the specified url
+ * @constructor
+ * @param {string} url Destination url
+ * @param {function(Object): Object=} preProcessFunction Optional function which can change object context before sending to the server 
+ */
+Metsuri.Transports.ServerSendTransport = function(url,preProcessFunction){}
+
+/**
+ * Will send data to the ElasticSearch server
+ * @constructor
+ * @param {string} host [http://localhost:9200]	Hostname (with port) to your ElasticSearch server
+ * @param {string} indexName [metsuri] Index name to be used for sending     
+ * @param {string} type [logs] Type to be used for sending
+ * @param {function(Object): Object=} preProcessFunction Optional function which can change object context before sending to the server 
+ */
+Metsuri.Transports.ElasticsearchTransport = function(host, indexName, type, preProcessFunction){}
