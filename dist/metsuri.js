911da8defc14c666471a3571108f307ee820d213
Artem Yarulin <Artem.Yarulin@fessguid.com>
2015-02-05 13:24:10 +0200

    0.0.3 release

--- dist/metsuri.js
+++ dist/metsuri.js
@@ -9,28 +9,9 @@ var Metsuri = function(minSeverity, transports) {
     'error': 4
   },
       _minSev = _severities[minSeverity],
-      _defTransport = (function(p1, p2, p3, p4, fullString) {
-        return console.log(fullString);
-      }),
+      _defTransport = new Metsuri.Transports.ConsoleTransport(),
       _curContext = {},
       _transports = transports instanceof Array ? transports : [_defTransport];
-  if (!_minSev)
-    throw new Error((_minSev + " - is not recognized severity level"));
-  var _sendLog = (function(sev, sender, action, context) {
-    if (_severities[sev] < _minSev)
-      return ;
-    context = context || {};
-    _mergeObjects({
-      sev: sev,
-      sender: sender,
-      action: action
-    }, context);
-    _mergeObjects(_curContext, context);
-    var fullString = ("[" + new Date().toISOString() + "] " + sev + " : " + sender + " " + action + " | " + JSON.stringify(context));
-    _transports.forEach((function(tr) {
-      return tr(sev, sender, action, context, fullString);
-    }));
-  });
   this.op = (function(opContext) {
     return _mergeObjects(opContext, _curContext);
   });
@@ -44,7 +25,88 @@ var Metsuri = function(minSeverity, transports) {
       return o2[key] = o1[key];
     }));
   });
+  var _sendLog = (function(sev, sender, action, context) {
+    if (_severities[sev] < _minSev)
+      return ;
+    context = context || {};
+    var date = new Date().toISOString();
+    _mergeObjects({
+      sev: sev,
+      sender: sender,
+      action: action,
+      date: date
+    }, context);
+    _mergeObjects(_curContext, context);
+    var fullString = ("[" + date + "] " + sev + " : " + sender + " " + action + " | " + JSON.stringify(context));
+    _transports.forEach((function(tr) {
+      return tr(sev, sender, action, context, fullString);
+    }));
+  });
+  if (!_minSev)
+    throw new Error((_minSev + " - is not recognized severity level"));
   Object.keys(_severities).forEach((function(sev) {
     return $__1[sev] = _sendLog.bind($__1, sev);
   }));
 };
+
+"use strict";
+Metsuri.Transports = (function() {
+  var nop = (function(input) {
+    return input;
+  });
+  var SendQueue = function(url) {
+    var _sendQueue = [],
+        _isSending = false,
+        _url = url;
+    this.sendMessage = (function(msg) {
+      _sendQueue.push(msg);
+      _sendNextMessage();
+    });
+    var _sendNextMessage = function() {
+      if (!_sendQueue.length || _isSending)
+        return ;
+      _isSending = true;
+      var msg = _sendQueue.shift(),
+          req = new XMLHttpRequest();
+      if (typeof msg !== 'string')
+        msg = JSON.stringify(msg);
+      req.open('POST', _url, true);
+      req.setRequestHeader('Content-Type', 'text/plain');
+      req.onreadystatechange = (function() {
+        if (req.readyState === 4) {
+          _isSending = false;
+          setTimeout(_sendNextMessage, 0);
+        }
+      });
+      req.send(msg);
+    };
+  };
+  var ConsoleTransport = function() {
+    var preProcessFunction = arguments[0] !== (void 0) ? arguments[0] : nop;
+    return (function(sev, sender, action, context, fullString) {
+      console.log(preProcessFunction(fullString));
+    });
+  };
+  var ServerSendTransport = function(url) {
+    var preProcessFunction = arguments[1] !== (void 0) ? arguments[1] : nop;
+    var _queue = new SendQueue(url);
+    return (function(sev, sender, action, context, fullString) {
+      _queue.sendMessage(preProcessFunction(context));
+    });
+  };
+  var ElasticsearchTransport = function() {
+    var host = arguments[0] !== (void 0) ? arguments[0] : 'localhost';
+    var indexName = arguments[1] !== (void 0) ? arguments[1] : 'metsuri';
+    var type = arguments[2] !== (void 0) ? arguments[2] : 'logs';
+    var preProcessFunction = arguments[3] !== (void 0) ? arguments[3] : nop;
+    var _queue = new SendQueue((host + "/" + indexName + "/" + type));
+    return (function(sev, sender, action, context, fullString) {
+      _queue.sendMessage(preProcessFunction(context));
+    });
+  };
+  return {
+    ConsoleTransport: ConsoleTransport,
+    ServerSendTransport: ServerSendTransport,
+    ElasticsearchTransport: ElasticsearchTransport
+  };
+})();
